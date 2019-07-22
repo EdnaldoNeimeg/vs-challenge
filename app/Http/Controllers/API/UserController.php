1d4cf751caab49c1a527268afb66873f7b3aeaa6
Ednaldo Neimeg <neimeg@gmail.com>
2019-07-22 14:37:04 -0300

    ...

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -34,7 +34,7 @@ class UserController extends Controller
             'name' => 'required', 
             'email' => 'required|email', 
             'password' => 'required', 
-            'c_password' => 'required|same:password', 
+            'password_confirmation' => 'required|same:password', 
         ]);
         if ($validator->fails()) { 
             return response()->json(['error'=>$validator->errors()], 401);            
